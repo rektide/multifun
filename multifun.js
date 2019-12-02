16096db16dd4316bf8170d55546f07e3aa434e74
jauntywunderkind <jaunty+wunder+kind+git@voodoowarez.com>
2019-12-02 15:35:51 -0500

    sketch out async handling

--- multifun.js
+++ multifun.js
@@ -1,7 +1,14 @@
 "use module"
 import Phases from "./phases.js"
 
-export function Multifun( fn, phases= Phases){
+function syncCall( phase, ctx){
+	
+}
+
+async function syncCall( phase, ctx){
+}
+
+export function Multifun( fn, opts= {}){
 	const
 	  name= typeof fn=== "string"? fn: fn&& fn.name|| "multifunc"
 	  fn= ({ [ name]: function( ...args){
@@ -12,14 +19,22 @@ export function Multifun( fn, phases= Phases){
 			fault: undefined
 		}
 		for( const let i= 0; i< phases.length; ++i){
-			phases.call( ctx, ...args)
+			let val= phases.call( ctx, ...args)
+			if( fn.async){
+				try{
+					val= await val
+				}catch( fault){
+					ctx.fault= fault
+				}
+			}
 			if( ctx.fault){
 				throw ctx.fault
 			}
 		}
 		return ctx.output
 	  })[ name]
-	fn.phases= phases
+	fn.phases= opts.phases|| Phases
+	fn.async= opts.async|| false
 	
 	return fn
 }

d148a7c0c565f0eca2c24e124634420a4f2feff3
jauntywunderkind <jaunty+wunder+kind+git@voodoowarez.com>
2019-12-02 15:47:42 -0500

    unrolled asyncNext

--- multifun.js
+++ multifun.js
@@ -1,33 +1,47 @@
 "use module"
 import Phases from "./phases.js"
 
-function syncCall( phase, ctx){
-	
+function syncNext(){
 }
 
-async function asyncCall( phase, ctx){
-	let val= phase.call( ctx, ...args)
+async function asyncNext( ctx){
+	// get phase, advance
+	ctx.phase= ctx.multifun.phases[ ctx.position]
+	++ctx.position
+
+	// run phase
 	try{
-		val= await val
+		ctx.output= phase.call( ctx, ...args)
+		// synchronously resolve before continuing
+		if( ctx.output&& ctx.output.then){
+			ctx.output= await ctx.output
+		}
 	}catch( fault){
 		ctx.fault= fault
 	}
 	if( ctx.fault){
 		throw ctx.fault
 	}
+	if( ctx.position>= ctx.multifun.phases.length){
+		return ctx.output
+	}
+	return asyncNext( ctx)
 }
 
 export function Multifun( fn, opts= {}){
 	const
 	  name= typeof fn=== "string"? fn: fn&& fn.name|| "multifunc"
 	  fn= ({ [ name]: function( ...args){
+		// I would love some convenient way to turn this into a history-preserving object
 		const ctx= {
 			args: args,
 			multifun: fn,
 			output: undefined,
 			fault: undefined
+			i: 0
 		}
-		for( const let i= 0; i< phases.length; ++i){
+		function next(){
+			
 		}
 		return ctx.output
 	  })[ name]
